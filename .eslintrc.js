032f49f8eb50ca2d2b1d83b1324f57b1e47be35e
David Mellen <d.mellen@remind.de>
2021-06-24 11:56:19 +0200

    Update eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        '@nuxtjs/eslint-config-typescript',
+        'plugin:prettier/recommended',
+        'plugin:nuxt/recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+    },
+    rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-inferrable-types': 'off',
+        'prettier/prettier': [
+            'error',
+            {
+                singleQuote: true,
+                semi: false,
+            },
+        ],
+        'no-console': [
+            'warn',
+            {
+                allow: ['warn', 'error'],
+            },
+        ],
+    },
+}
