5f9b830dd859362ab952d24789b4162aa5f83762
David Mellen <d.mellen@remind.de>
2021-07-07 13:51:36 +0200

    Use hx tags for ce-header

--- src/components/content/elements/CeHeader/CeHeader.ts
+++ src/components/content/elements/CeHeader/CeHeader.ts
@@ -13,13 +13,7 @@ export default class CeHeader extends mixins(baseCe) implements baseCe {
     subheader!: string
 
     get visible(): boolean {
-        return this.headerLayout !== 100
-    }
-
-    get headerAlignment(): string {
-        return this.headerPosition
-            ? `ce-header__main--${this.headerPosition}`
-            : ''
+        return this.headerLayout !== 100 && (!!this.header || !!this.subheader)
     }
 
     get subHeaderTop(): boolean {
@@ -29,30 +23,62 @@ export default class CeHeader extends mixins(baseCe) implements baseCe {
         )
     }
 
+    get tag(): string {
+        return this.headerLayout > 0 && this.headerLayout <= 6
+            ? `h${this.headerLayout}`
+            : 'div'
+    }
+
+    private renderLink(createElement: CreateElement): VNode {
+        const tag = this.headerLink.type === 'page' ? 'nuxt-link' : 'a'
+        const linkAttrName = this.headerLink.type === 'page' ? 'to' : 'href'
+
+        return createElement(
+            tag,
+            {
+                attrs: {
+                    [linkAttrName]: this.headerLink.url,
+                    target: this.headerLink.target,
+                },
+            },
+            this.header
+        )
+    }
+
+    private renderHeader(createElement: CreateElement): VNode {
+        return createElement(
+            this.tag,
+            {
+                class: [
+                    'ce-header__main',
+                    {
+                        [`ce-header__main--${this.headerPosition}`]: this
+                            .headerPosition,
+                    },
+                ],
+            },
+            this.headerLink ? [this.renderLink(createElement)] : this.header
+        )
+    }
+
+    private renderSubHeader(createElement: CreateElement): VNode {
+        return createElement('div', { class: 'ce-header__sub' }, this.subheader)
+    }
+
     render(createElement: CreateElement): VNode {
         if (this.visible) {
-            const main = createElement(
-                this.headerLink ? 'nuxt-link' : 'div',
-                {
-                    class: ['ce-header__main', this.headerAlignment],
-                    attrs: this.headerLink
-                        ? {
-                              to: this.headerLink.url,
-                          }
-                        : {},
-                },
-                this.header
-            )
-            const sub = createElement(
-                'div',
-                { class: 'ce-header__sub' },
-                this.subheader
-            )
-
             return createElement(
-                'div',
+                'header',
                 { class: 'ce-header' },
-                this.subHeaderTop ? [sub, main] : [main, sub]
+                this.subHeaderTop
+                    ? [
+                          this.renderSubHeader(createElement),
+                          this.renderHeader(createElement),
+                      ]
+                    : [
+                          this.renderHeader(createElement),
+                          this.renderSubHeader(createElement),
+                      ]
             )
         } else {
             return createElement()
