90b7697c8a71903f76c096ac2ff426b361fb6aa1
David Mellen <d.mellen@remind.de>
2021-09-27 15:27:17 +0200

    Add css class to html-parser
    RENUXTTYPO-13

---
+++ src/components/util/HtmlParser.vue
@@ -0,0 +1,9 @@
+<template>
+    <!-- Just add a css class to allow easier styling -->
+    <!-- eslint-disable-next-line vue/no-v-html -->
+    <div class="html-content" v-html="content" />
+</template>
+
+<script>
+export { default } from 'nuxt-typo3/lib/templates/components/utilities/HtmlParser.vue'
+</script>
--- src/plugins/components.ts
+++ src/plugins/components.ts
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import { Plugin } from '@nuxt/types'
 
+import HtmlParser from '~rmnd-typo3/components/util/HtmlParser.vue'
 import * as components from '~rmnd-typo3/components'
 
 const plugin: Plugin = ({ app }) => {
-    Object.keys(components).forEach((key) => {
+    Object.keys({ ...components, HtmlParser }).forEach((key) => {
         if (app.components) {
             Vue.component(key, components[key])
             app.components[key] = components[key]
