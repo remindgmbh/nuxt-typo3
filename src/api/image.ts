021d7377e873a610a7fb61d7d4cc29fb82c3149b
David Mellen <d.mellen@remind.de>
2021-11-08 12:12:20 +0100

    Use frontend API path instead of backend publicUrl
    RENUXTTYPO-22

--- src/api/image.ts
+++ src/api/image.ts
@@ -7,6 +7,7 @@ export interface Image {
         alternative?: string
         title?: string
         link?: string
+        originalUrl: string
     }
     publicUrl: string
 }
--- src/components/util/RImage.ts
+++ src/components/util/RImage.ts
@@ -1,33 +1,36 @@
-import Vue, { PropType } from 'vue'
+import Vue, { PropType, CreateElement, VNode } from 'vue'
+import combineURLs from 'axios/lib/helpers/combineURLs'
 import { Image } from '../../api'
 
 export default Vue.extend({
     name: 'RImage',
-    functional: true,
     props: {
         file: {
             type: Object as PropType<Image>,
             required: true,
         },
     },
-    render(createElement, context) {
-        function renderImage() {
+    render(createElement: CreateElement): VNode {
+        const renderImage = () => {
             return createElement('img', {
                 attrs: {
-                    src: context.props.file.publicUrl,
-                    height: context.props.file.properties.dimensions.height,
-                    width: context.props.file.properties.dimensions.width,
-                    alt: context.props.file.properties.alternative,
-                    title: context.props.file.properties.title,
+                    src: combineURLs(
+                        this.$typo3.options.api.baseURL,
+                        this.file.properties.originalUrl
+                    ),
+                    height: this.file.properties.dimensions.height,
+                    width: this.file.properties.dimensions.width,
+                    alt: this.file.properties.alternative,
+                    title: this.file.properties.title,
                 },
             })
         }
 
-        return context.props.file
-            ? context.props.file.properties.link
+        return this.file
+            ? this.file.properties.link
                 ? createElement(
                       'nav-link',
-                      { attrs: { to: context.props.file.properties.link } },
+                      { attrs: { to: this.file.properties.link } },
                       [renderImage()]
                   )
                 : renderImage()
