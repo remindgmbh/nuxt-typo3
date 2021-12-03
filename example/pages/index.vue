ff4683e00f481c26e792c008367c137a1c943ed8
David Mellen <d.mellen@remind.de>
2021-12-03 09:54:01 +0100

    Add example for tabs
    RENUXTTYPO-29

--- example/pages/index.vue
+++ example/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
+    <div class="examples">
         <nuxt-link to="/menu">menu</nuxt-link>
+        <nuxt-link to="/tabs">tabs</nuxt-link>
     </div>
 </template>
 
@@ -10,3 +11,10 @@ import { Component, Vue } from 'vue-property-decorator'
 @Component
 export default class Index extends Vue {}
 </script>
+
+<style lang="scss">
+.examples {
+    display: flex;
+    flex-direction: column;
+}
+</style>
---
+++ example/pages/tabs.vue
@@ -0,0 +1,39 @@
+<template>
+    <r-tabs :items="items">
+        <template #title="{ item }">{{ item.title }}</template>
+        <template #content="{ item }">{{ item.content }}</template>
+    </r-tabs>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Tabs extends Vue {
+    items = [
+        { title: 'item-1', content: 'content for item-1' },
+        { title: 'item-2', content: 'content for item-2' },
+    ]
+}
+</script>
+<style lang="scss">
+.r-tabs {
+    background-color: lightgrey;
+
+    &__link {
+        cursor: pointer;
+        background-color: transparent;
+        border: none;
+        padding: 1rem;
+
+        &--active {
+            background-color: grey;
+        }
+    }
+
+    &__content {
+        background-color: grey;
+        padding: 1rem;
+    }
+}
+</style>
