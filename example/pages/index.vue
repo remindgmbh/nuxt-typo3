85bb8db8b98fab0c8b268a98f88ce05040ca520f
David Mellen <d.mellen@remind.de>
2022-01-27 10:55:15 +0100

    Add modal examples
    RENUXTTYPO-29

--- example/pages/index.vue
+++ example/pages/index.vue
@@ -2,6 +2,8 @@
     <div class="examples">
         <nuxt-link to="/accordion">accordion</nuxt-link>
         <nuxt-link to="/menu">menu</nuxt-link>
+        <nuxt-link to="/modal">modal</nuxt-link>
+        <nuxt-link to="/modal-fullscreen">modal-fullscreen</nuxt-link>
         <nuxt-link to="/scroll-indicator">scroll-indicator</nuxt-link>
         <nuxt-link to="/scroll-indicator-component"
             >scroll-indicator-component</nuxt-link
---
+++ example/pages/modal-fullscreen.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="modal-fullscreen-example">
+        <button @click="modalVisible = true">Open Modal</button>
+        <t3-modal v-model="modalVisible">
+            <t3-modal-content class="modal-fullscreen-example__content">
+                <button
+                    class="modal-fullscreen-example__close"
+                    @click="modalVisible = false"
+                >
+                    x
+                </button>
+                <div>content top</div>
+                <div>content bottom</div>
+            </t3-modal-content>
+        </t3-modal>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class ModalFullscreenExample extends Vue {
+    modalVisible = false
+}
+</script>
+<style lang="scss">
+.modal-fullscreen-example {
+    .modal-content-transition {
+        &-enter-active,
+        &-leave-active {
+            transition: transform 1s;
+        }
+
+        &-enter,
+        &-leave-to {
+            transform: translateX(calc(50vw + 50%));
+        }
+
+        &-enter-to,
+        &-leave {
+            transform: translateX(0);
+        }
+    }
+
+    &__close {
+        position: absolute;
+        right: 0.25rem;
+        top: 0.25rem;
+    }
+
+    &__content {
+        position: relative;
+        background-color: grey;
+        padding: 2rem;
+        width: 100%;
+        height: 110vh;
+        margin: auto;
+        display: flex;
+        justify-content: space-between;
+        flex-direction: column;
+    }
+}
+</style>
