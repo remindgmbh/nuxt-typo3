280513d6e97e4278f6122004c3373b1011d04a50
David Mellen <d.mellen@remind.de>
2022-07-07 10:35:38 +0200

    Add header to solr content element component
    RENUXTTYPO-52

--- src/runtime/components/content/elements/solr/T3CeSolrPiSearch.vue
+++ src/runtime/components/content/elements/solr/T3CeSolrPiSearch.vue
@@ -1,20 +1,23 @@
 <template>
-    <form class="t3-ce-solr-search" @submit="submit">
-        <T3Autocomplete
-            class="t3-ce-solr-search__input"
-            :disabled="loading"
-            :name="inputName"
-            :placeholder="placeholder"
-            :default-value="defaultValue"
-            :option-groups="optionGroups"
-            @input="onInput"
-        />
-        <button class="t3-ce-solr-search__submit" type="submit">
-            <span class="t3-ce-solr-search__submit-label">{{
-                submitLabel
-            }}</span>
-        </button>
-    </form>
+    <div class="t3-ce-solr-search">
+        <T3CeHeader :content-element="contentElement" />
+        <form class="t3-ce-solr-search__form" @submit="submit">
+            <T3Autocomplete
+                class="t3-ce-solr-search__input"
+                :disabled="loading"
+                :name="inputName"
+                :placeholder="placeholder"
+                :default-value="defaultValue"
+                :option-groups="optionGroups"
+                @input="onInput"
+            />
+            <button class="t3-ce-solr-search__submit" type="submit">
+                <span class="t3-ce-solr-search__submit-label">{{
+                    submitLabel
+                }}</span>
+            </button>
+        </form>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -40,7 +43,9 @@ const {
 
 <style lang="scss">
 .t3-ce-solr-search {
-    display: flex;
+    &__form {
+        display: flex;
+    }
 
     &__input {
         flex-grow: 1;
