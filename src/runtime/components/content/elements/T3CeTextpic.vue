02c468130882b38bc8577ebd3cdcece2561eb178
David Mellen <d.mellen@remind.de>
2022-07-12 14:29:23 +0200

    Remove duplicated styles in textpic component

--- src/runtime/components/content/elements/T3CeTextpic.vue
+++ src/runtime/components/content/elements/T3CeTextpic.vue
@@ -26,37 +26,3 @@ const contentElement: Api.ContentElement<Api.Content.Textmedia> = {
     },
 }
 </script>
-
-<style lang="scss">
-.t3-ce-textmedia {
-    display: flex;
-    flex-wrap: wrap;
-
-    &__asset {
-        width: 50%;
-
-        &--small {
-            width: calc(1 / 3 * 100%);
-        }
-
-        &--right {
-            order: 1;
-        }
-
-        img {
-            height: 100%;
-            width: 100%;
-            object-fit: contain;
-            object-position: top;
-        }
-    }
-
-    &__text {
-        width: 50%;
-
-        &--large {
-            width: calc(2 / 3 * 100%);
-        }
-    }
-}
-</style>
