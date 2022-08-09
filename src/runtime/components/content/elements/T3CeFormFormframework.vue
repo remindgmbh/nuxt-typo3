d6f35d45bb6bedd4905a8cb0773fd9392821e02f
David Mellen <d.mellen@remind.de>
2022-08-09 14:00:01 +0200

    Add loading to form content element

--- src/runtime/components/content/elements/T3CeFormFormframework.vue
+++ src/runtime/components/content/elements/T3CeFormFormframework.vue
@@ -10,6 +10,7 @@
         <div :class="`t3-ce-form__form`">
             <T3Form
                 :form-elements="formElements"
+                :loading="loading"
                 :submit-label="contentElement.content.form.i18n.submit"
                 @submit="submit"
             />
@@ -18,6 +19,7 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import { useRouter } from '#app'
 import { Api, useApi, useCeFormFormframework } from '#nuxt-typo3'
 
@@ -28,8 +30,10 @@ const props = defineProps<{
 const api = useApi()
 const router = useRouter()
 const { formElements } = useCeFormFormframework(props.contentElement.content)
+const loading = ref(false)
 
 async function submit(data: Record<string, any>) {
+    loading.value = true
     const body = new FormData()
     body.set('responseElementId', props.contentElement.id.toString())
 
@@ -37,19 +41,23 @@ async function submit(data: Record<string, any>) {
         body.set(key, data[key] ?? '')
     })
 
-    const result = await api.post<
-        Api.ContentElement<Api.Content.Formframework>
-    >(props.contentElement.content.link.href, { body })
+    try {
+        const result = await api.post<
+            Api.ContentElement<Api.Content.Formframework>
+        >(props.contentElement.content.link.href, { body })
 
-    if (typeof result.content.form === 'string') {
-        console.error(result.content.form)
-    } else if (
-        result.content.form.api.status === 'success' &&
-        result.content.form.api.actionAfterSuccess
-    ) {
-        router.push({
-            path: result.content.form.api.actionAfterSuccess.redirectUrl,
-        })
+        if (typeof result.content.form === 'string') {
+            console.error(result.content.form)
+        } else if (
+            result.content.form.api.status === 'success' &&
+            result.content.form.api.actionAfterSuccess
+        ) {
+            router.push({
+                path: result.content.form.api.actionAfterSuccess.redirectUrl,
+            })
+        }
+    } finally {
+        loading.value = false
     }
 }
 </script>
