aeb410e94e246744a5cb11c3a9532bd42514b5bf
David Mellen <d.mellen@remind.de>
2023-03-17 09:25:42 +0100

    Add active slot prop to menu trigger

--- src/runtime/components/ui/T3Menu/T3MenuTrigger.vue
+++ src/runtime/components/ui/T3Menu/T3MenuTrigger.vue
@@ -5,13 +5,13 @@
         class="t3-menu-trigger"
         @click="toggle"
     >
-        <slot />
+        <slot :active="activeItemId === id" />
     </component>
 </template>
 
 <script setup lang="ts">
 import { inject } from 'vue'
-import { setActiveItemIdSymbol } from './shared'
+import { activeItemIdSymbol, setActiveItemIdSymbol } from './shared'
 
 const props = withDefaults(
     defineProps<{
@@ -23,6 +23,7 @@ const props = withDefaults(
 )
 
 const setActiveItemId = inject(setActiveItemIdSymbol)
+const activeItemId = inject(activeItemIdSymbol)
 
 function toggle(e: MouseEvent): void {
     e.stopPropagation()
