920a8768d3edad25671649e44b9df73a85a17fb4
David Mellen <d.mellen@remind.de>
2023-03-24 09:04:34 +0100

    Add title attribute to t3-link
    RENUXTTYPO-121

--- src/runtime/components/content/helper/T3Link.vue
+++ src/runtime/components/content/helper/T3Link.vue
@@ -1,5 +1,10 @@
 <template>
-    <NuxtLink class="t3-link" :to="to.href" :target="to.target">
+    <NuxtLink
+        class="t3-link"
+        :to="to.href"
+        :target="to.target"
+        :title="to.title"
+    >
         <slot>{{ to.title }}</slot>
     </NuxtLink>
 </template>
