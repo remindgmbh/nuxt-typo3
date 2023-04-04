7c871c39e2f646f6ada76154aac568a6b04dae8a
David Mellen <d.mellen@remind.de>
2023-04-04 13:24:21 +0200

    Add v-model to menu component
    RENUXTTYPO-125

--- src/runtime/components/ui/T3Menu.vue
+++ src/runtime/components/ui/T3Menu.vue
@@ -10,15 +10,16 @@ import { useRoute } from '#app'
 
 const props = withDefaults(
     defineProps<{
+        modelValue?: string
         items: any[]
         tag?: keyof HTMLElementTagNameMap
         idField?: string
     }>(),
-    { tag: 'nav', idField: 'id' }
+    { tag: 'nav', idField: 'id', modelValue: undefined }
 )
 
 const emit = defineEmits<{
-    (e: 'change', id?: string): void
+    (e: 'update:modelValue', value?: string): void
 }>()
 
 const activeItemId = ref<string>()
@@ -27,17 +28,22 @@ const route = useRoute()
 
 function close() {
     activeItemId.value = undefined
-    emit('change')
 }
 
 function toggle(id: string) {
     activeItemId.value = activeItemId.value === id ? undefined : id
-    emit('change', activeItemId.value)
 }
 
 const activeItem = computed(() =>
     props.items.find((item) => item[props.idField] === activeItemId?.value)
 )
+
+watch(
+    () => props.modelValue,
+    (value) => (activeItemId.value = value)
+)
+
+watch(activeItemId, (value) => emit('update:modelValue', value))
 
 watch(route, () => (activeItemId.value = undefined))
 </script>
