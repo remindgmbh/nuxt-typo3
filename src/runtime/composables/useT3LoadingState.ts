bd41772920db32e079e9aa5e71a95d3c83a75ca0
David Mellen <d.mellen@remind.de>
2023-04-13 10:27:41 +0200

    Fix loading-state type in dist
    RENUXTTYPO-111

--- src/runtime/composables/useT3LoadingState.ts
+++ src/runtime/composables/useT3LoadingState.ts
@@ -1,8 +1,15 @@
+import { Ref } from 'vue'
 import { useState } from '#imports'
 
 export function useT3LoadingState() {
-    const loadingData = useState<boolean>('loadingData', () => false)
-    const loadingPage = useState<boolean>('loadingPage', () => false)
+    const loadingData: Ref<boolean> = useState<boolean>(
+        'loadingData',
+        () => false
+    )
+    const loadingPage: Ref<boolean> = useState<boolean>(
+        'loadingPage',
+        () => false
+    )
 
     return {
         loadingData,
