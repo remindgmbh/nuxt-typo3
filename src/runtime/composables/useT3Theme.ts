4c95556122c42626e7d97bb3da52a9b4ec73f363
David Mellen <d.mellen@remind.de>
2023-04-14 12:55:37 +0200

    Fix error in theme composable

--- src/runtime/composables/useT3Theme.ts
+++ src/runtime/composables/useT3Theme.ts
@@ -3,15 +3,15 @@ import { Color } from '../../module'
 import { useState, useT3Config } from '#imports'
 
 export function useT3Theme() {
-    const { theme } = useT3Config()
+    const { themes, defaultTheme } = useT3Config()
 
     const selectedTheme: Ref<string> = useState<string>(
         'theme',
-        () => theme.default
+        () => defaultTheme
     )
 
     const colors = computed<{ [colorName: string]: Color }>(
-        () => theme.themes[selectedTheme.value]
+        () => themes[selectedTheme.value]
     )
 
     return {
