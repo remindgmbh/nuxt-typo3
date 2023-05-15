8d2bd49e2d8056caf994901da6bac95626101215
David Mellen <d.mellen@remind.de>
2023-05-15 11:19:57 +0200

    Add root backend layout

---
+++ src/runtime/components/backend-layouts/T3BlRoot.vue
@@ -0,0 +1,12 @@
+<template>
+    <T3BlDefault :content="content" :breadcrumbs="breadcrumbs" />
+</template>
+
+<script setup lang="ts">
+import { T3Model } from '#imports'
+
+defineProps<{
+    content: { [colPos: string]: T3Model.Typo3.Content.Element[] }
+    breadcrumbs: T3Model.Typo3.Breadcrumb[]
+}>()
+</script>
