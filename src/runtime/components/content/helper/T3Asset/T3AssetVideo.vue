c750fe536e67ce074af967d4d6b1409f7ac9d29b
David Mellen <d.mellen@remind.de>
2023-06-02 15:10:49 +0200

    Pass asset attrs to asset-video component

--- src/runtime/components/content/helper/T3Asset/T3AssetVideo.vue
+++ src/runtime/components/content/helper/T3Asset/T3AssetVideo.vue
@@ -3,11 +3,14 @@
         :src="file.publicUrl"
         :autoplay="!!file.properties.autoplay"
         :preload="file.properties.lazyLoading ? 'none' : undefined"
-        controls
+        v-bind="assetAttrs"
     ></video>
 </template>
 <script setup lang="ts">
 import { T3Model } from '#imports'
 
-defineProps<{ file: T3Model.Typo3.Asset }>()
+defineProps<{
+    file: T3Model.Typo3.Asset
+    assetAttrs?: { [key: string]: any }
+}>()
 </script>
