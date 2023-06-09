ae3e09dcef18827e025df34b39a8f109b03e3fa9
David Mellen <d.mellen@remind.de>
2023-06-09 15:17:47 +0200

    Adapt to changes in rmnd_extbase

--- src/runtime/models/typo3/extbase/filterableList.ts
+++ src/runtime/models/typo3/extbase/filterableList.ts
@@ -2,7 +2,7 @@ import { Filter, Pagination } from '.'
 
 export interface FilterableList<T> {
     count: number
-    countWithoutLimit?: number
+    countWithoutLimit: number
     items: T[]
     pagination: Pagination
     filters: Filter[]
