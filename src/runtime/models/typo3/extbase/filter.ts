a586f124db9526b9112925d17d3c648641b3e7d6
David Mellen <d.mellen@remind.de>
2023-09-25 16:00:06 +0200

    Add prefix and suffix to filter model

--- src/runtime/models/typo3/extbase/filter.ts
+++ src/runtime/models/typo3/extbase/filter.ts
@@ -3,6 +3,8 @@ import { FilterValue } from '.'
 export interface Filter {
     name: string
     label: string
+    prefix: string
+    suffix: string
     values: FilterValue[]
     allValues: {
         link: string
