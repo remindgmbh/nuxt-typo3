64e7af30b41fce706eef2619d614a50037f3c26e
David Mellen <d.mellen@remind.de>
2024-02-07 13:40:56 +0100

    fix: return undefined if footer content is not set

--- src/runtime/composables/useT3Footer.ts
+++ src/runtime/composables/useT3Footer.ts
@@ -4,8 +4,10 @@ import { useT3Data } from '#imports'
 export function useT3Footer<T = unknown>() {
     const { currentInitialData } = useT3Data()
 
-    const footer = computed(
-        () => currentInitialData.value?.footer as T | undefined,
+    const footer = computed<T | undefined>(() =>
+        currentInitialData.value?.footer
+            ? (currentInitialData.value?.footer as T)
+            : undefined,
     )
 
     return footer
