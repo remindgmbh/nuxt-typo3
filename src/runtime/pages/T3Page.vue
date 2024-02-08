e674276c1baf6eafdbe1b484da55947098e66377
David Mellen <d.mellen@remind.de>
2024-02-08 13:53:56 +0100

    fix: use currentPageData in t3-page to prevent nuxt warning
    RENUXTTYPO-164

--- src/runtime/pages/T3Page.vue
+++ src/runtime/pages/T3Page.vue
@@ -1,10 +1,6 @@
 <template>
     <div class="t3-page">
-        <component
-            :is="BackendLayout"
-            v-if="currentPageData"
-            :page-data="currentPageData"
-        />
+        <component :is="BackendLayout" v-if="pageData" :page-data="pageData" />
         <T3PageError
             v-else
             :error="pageError ?? { statusText: t('unexpectedError') }"
@@ -13,43 +9,24 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue'
-import {
-    useRoute,
-    useT3Data,
-    useT3DynamicComponent,
-    useT3PageHead,
-} from '#imports'
+import { useT3Data, useT3DynamicComponent, useT3PageHead } from '#imports'
 import { T3BlDefault } from '#components'
-import { onBeforeRouteUpdate } from 'vue-router'
+import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 
 const { t } = useI18n()
 
-// when the route changes the old page remains visible until the new page is loaded
-// using the computed currentPagePath the old page content is not available anymore
-// so currentPagePath is only set during created hook and when only the route query
-// params (e.g. for solr) change
-const currentPagePath = ref<string>(useRoute().fullPath)
-const { pageData, pageError } = useT3Data()
+const { currentPageData, pageError } = useT3Data()
 
-const currentPageData = computed(
-    () => pageData.value[currentPagePath.value] ?? pageError.value?.data,
-)
+const pageData = computed(() => currentPageData.value ?? pageError.value?.data)
 
-if (currentPageData.value) {
-    useT3PageHead(currentPageData.value)
+if (pageData.value) {
+    useT3PageHead(pageData.value)
 }
 
 const BackendLayout = useT3DynamicComponent(
-    currentPageData.value?.appearance.backendLayout,
+    pageData.value?.appearance.backendLayout,
     'Bl',
     T3BlDefault,
 )
-
-onBeforeRouteUpdate((to, from) => {
-    if (from.path === to.path) {
-        currentPagePath.value = to.fullPath
-    }
-})
 </script>
