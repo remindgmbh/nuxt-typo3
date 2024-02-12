9c9e7c570974c84e8588990fd57e29f3f79c5be9
David Mellen <d.mellen@remind.de>
2024-02-12 10:58:40 +0100

    feat: add pagination prev, next and pages attributes as props

--- src/runtime/components/typo3/content/helper/T3Pagination.vue
+++ src/runtime/components/typo3/content/helper/T3Pagination.vue
@@ -6,6 +6,7 @@
             class="t3-pagination__navigation t3-pagination__navigation--prev"
             :class="{ 't3-pagination__navigation--disabled': !pagination.prev }"
             :to="pagination.prev"
+            v-bind="navigationAttributes.prev"
         >
             <slot name="prev"></slot>
         </component>
@@ -18,6 +19,7 @@
                         't3-pagination__page--disabled': page.active,
                     }"
                     :to="page.link"
+                    v-bind="pageAttributes(page)"
                 >
                     <slot name="page" :page="page">{{ page.pageNumber }}</slot>
                 </component>
@@ -30,6 +32,7 @@
             class="t3-pagination__navigation t3-pagination__navigation--next"
             :class="{ 't3-pagination__navigation--disabled': !pagination.next }"
             :to="pagination.next"
+            v-bind="navigationAttributes.next"
         >
             <slot name="next"></slot>
         </component>
@@ -41,11 +44,26 @@ import { T3Model, useT3Pagination } from '#imports'
 import { NuxtLink } from '#components'
 import { toRef } from 'vue'
 
-const props = defineProps<{
-    disableNavigation?: boolean
-    numberOfPages?: number
-    pagination: T3Model.Typo3.Extbase.Pagination
-}>()
+const props = withDefaults(
+    defineProps<{
+        disableNavigation?: boolean
+        navigationAttributes?: {
+            next?: object
+            prev?: object
+        }
+        numberOfPages?: number
+        pagination: T3Model.Typo3.Extbase.Pagination
+        pageAttributes?: (page: T3Model.Typo3.Extbase.PaginationPage) => object
+    }>(),
+    {
+        navigationAttributes: () => ({
+            next: {},
+            prev: {},
+        }),
+        numberOfPages: undefined,
+        pageAttributes: () => ({}),
+    },
+)
 
 const { pages, showDivider } = useT3Pagination(
     toRef(() => props.pagination),
