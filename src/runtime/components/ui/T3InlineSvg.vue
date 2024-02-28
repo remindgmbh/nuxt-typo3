2e5d32698a936228a7489abb2c4cbdf7db965056
David Mellen <d.mellen@remind.de>
2024-02-28 08:57:19 +0100

    feat: add inline-svg component

---
+++ src/runtime/components/ui/T3InlineSvg.vue
@@ -0,0 +1,52 @@
+<template>
+    <ClientOnly>
+        <!-- eslint-disable-next-line vue/no-v-html -->
+        <svg :id="id" v-bind="attributes" v-html="html"></svg>
+    </ClientOnly>
+</template>
+
+<script setup lang="ts">
+import { computed, onMounted, ref, watch } from 'vue'
+import { compileString } from 'sass'
+
+const props = defineProps<{
+    src: string
+}>()
+
+const html = ref<string>()
+const attributes = ref<{ [key: string]: string }>({})
+
+const id = computed(() => `svg-${crypto.randomUUID()}`)
+
+onMounted(async () => {
+    await loadSvg()
+})
+
+async function loadSvg() {
+    const blob = await $fetch<Blob>(props.src)
+    const text = await blob.text()
+
+    const parser = new DOMParser()
+    const { documentElement } = parser.parseFromString(text, 'image/svg+xml')
+
+    for (const attribute of documentElement.attributes) {
+        if (attribute.name === 'id') {
+            continue
+        }
+        attributes.value[attribute.name] = attribute.value
+    }
+
+    const styleElements = documentElement.getElementsByTagName('style')
+
+    for (const styleElement of styleElements) {
+        const { css } = compileString(
+            `#${id.value} { ${styleElement.innerHTML} }`,
+        )
+        styleElement.innerHTML = css
+    }
+
+    html.value = documentElement.innerHTML
+}
+
+watch(() => props.src, loadSvg)
+</script>
