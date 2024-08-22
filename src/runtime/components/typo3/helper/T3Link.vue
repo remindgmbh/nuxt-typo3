1be61fdd8e9a053659490e6ecf70c0a70588ee5b
David Mellen <d.mellen@remind.de>
2024-08-22 14:49:41 +0200

    feat: allow string for to-prop in link

--- src/runtime/components/typo3/helper/T3Link.vue
+++ src/runtime/components/typo3/helper/T3Link.vue
@@ -1,18 +1,30 @@
 <template>
-    <NuxtLink
-        class="t3-link"
-        :target="to?.target"
-        :title="to?.title"
-        :to="to?.href"
-    >
-        <slot>{{ to?.linkText }}</slot>
+    <NuxtLink class="t3-link" :target="target" :title="title" :to="href">
+        <slot>{{ linkText }}</slot>
     </NuxtLink>
 </template>
 
 <script setup lang="ts">
 import { type T3Model } from '#imports'
+import { computed } from 'vue'
 
-defineProps<{
-    to?: T3Model.Typo3.TypoLink
+const props = defineProps<{
+    to?: T3Model.Typo3.TypoLink | string
 }>()
+
+const href = computed(
+    () => (typeof props.to === 'string' ? props.to : props.to?.href) ?? '',
+)
+
+const title = computed(() =>
+    typeof props.to !== 'string' ? props.to?.title : undefined,
+)
+
+const target = computed(() =>
+    typeof props.to !== 'string' ? props.to?.target : undefined,
+)
+
+const linkText = computed(() =>
+    typeof props.to !== 'string' ? props.to?.linkText : undefined,
+)
 </script>
