209d95299f3348cfc162abf537ae5a4289073d32
David Mellen <d.mellen@remind.de>
2024-09-25 08:11:09 +0200

    refactor: cleanup breakpoint composable

--- src/runtime/composables/useT3Breakpoints.ts
+++ src/runtime/composables/useT3Breakpoints.ts
@@ -3,36 +3,32 @@ import containerWidths from '../assets/styles/js/containerWidths.module.scss'
 import screenWidths from '../assets/styles/js/screenWidths.module.scss'
 
 export function useT3Breakpoints() {
-    const getPxValue = (value: string) => {
+    const valueToPx = (value: string) => {
         const regex = /^\d+r?em$/
         const multiplier = regex.test(value) ? 16 : 1
         return Number.parseInt(value) * multiplier
     }
 
-    const screenWidthsInPx = computed<{ [key: string]: number }>(() =>
-        Object.entries(screenWidths).reduce(
+    const objectToPx = (o: { [key: string]: string }) =>
+        Object.entries(o).reduce(
             (result, [key, value]) => {
-                result[key] = getPxValue(value)
+                result[key] = valueToPx(value)
                 return result
             },
             {} as { [key: string]: number },
-        ),
+        )
+
+    const screenWidthsInPx = computed<{ [key: string]: number }>(() =>
+        objectToPx(screenWidths),
     )
 
     const containerWidthsInPx = computed<{ [key: string]: number }>(() =>
-        Object.entries(containerWidths).reduce(
-            (result, [key, value]) => {
-                result[key] = getPxValue(value)
-                return result
-            },
-            {} as { [key: string]: number },
-        ),
+        objectToPx(containerWidths),
     )
 
     const breakpointsAsc = computed<string[]>(() =>
         Object.keys(screenWidthsInPx.value).sort(
-            (a, b) =>
-                containerWidthsInPx.value[a] - containerWidthsInPx.value[b],
+            (a, b) => screenWidthsInPx.value[a] - screenWidthsInPx.value[b],
         ),
     )
 
