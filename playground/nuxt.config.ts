c4382252d636f5e07bc80e4d454fd25f07208d47
David Mellen <d.mellen@remind.de>
2024-10-10 13:28:03 +0200

    fix: remove cookiebotUid in playground nuxt config

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -20,7 +20,6 @@ export default defineNuxtConfig({
                     baseUrl: '',
                 },
                 baseUrl: '',
-                cookiebotUid: '',
             },
         },
     },
