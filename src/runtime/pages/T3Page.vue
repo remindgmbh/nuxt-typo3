8e24a32f5a9e6697ad984964ec71bd4e06efc758
David Mellen <d.mellen@remind.de>
2024-10-16 11:51:42 +0200

    fix: add html lang attribute

--- src/runtime/pages/T3Page.vue
+++ src/runtime/pages/T3Page.vue
@@ -9,19 +9,36 @@
 </template>
 
 <script setup lang="ts">
-import { useHead, useT3Data, useT3DynamicComponent } from '#imports'
+import {
+    useHead,
+    useT3Data,
+    useT3DynamicComponent,
+    useT3Languages,
+} from '#imports'
+import type { MetaObject } from '@nuxt/schema'
 import { T3BlDefault } from '#components'
 import { computed } from 'vue'
+import defu from 'defu'
 import { useI18n } from 'vue-i18n'
 
 const { t } = useI18n()
 
 const { currentPageData, pageError } = useT3Data()
+const { activeLanguage } = useT3Languages()
 
 const pageData = computed(() => currentPageData.value ?? pageError.value?.data)
 
 if (pageData.value) {
-    useHead(pageData.value.seo)
+    const headData: MetaObject = defu(
+        {
+            htmlAttrs: {
+                lang: activeLanguage.value?.twoLetterIsoCode,
+            },
+        },
+        pageData.value.seo,
+    )
+
+    useHead(headData)
 }
 
 const BackendLayout = useT3DynamicComponent(
