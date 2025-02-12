4571ba45ab1378bf9fa8a342a554f0621ff35d16
David Mellen <d.mellen@remind.de>
2025-02-12 08:54:40 +0100

    feat: add debounce composable

---
+++ src/runtime/composables/useT3Debounce.ts
@@ -0,0 +1,29 @@
+import { ref } from 'vue'
+
+export function useT3Debounce<T>(
+    callback: (...args: any[]) => PromiseLike<T>,
+    timeout = 500,
+) {
+    let timer: NodeJS.Timeout | undefined
+    const promise = ref<PromiseLike<T>>()
+
+    const debounce = (...args: any[]): PromiseLike<unknown> => {
+        promise.value = new Promise((resolve, reject) => {
+            clearTimeout(timer)
+            timer = setTimeout(() => {
+                try {
+                    resolve(callback(...args))
+                } catch (error) {
+                    reject(error)
+                }
+            }, timeout)
+        })
+
+        return promise.value
+    }
+
+    return {
+        debounce,
+        promise,
+    }
+}
